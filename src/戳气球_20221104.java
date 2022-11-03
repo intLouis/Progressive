ba9a8bf22244293c3ba09f248fb78598b70653d7
intLouis <378435665@qq.com>
2022-11-04 05:52:49 +0800

    20221104:hard:戳气球

--- src/戳气球_20221104.java
+++ src/戳气球_20221104.java
@@ -1,2 +1,30 @@
 public class 戳气球_20221104 {
+    public int maxCoins(int[] nums) {
+        int len = nums.length;
+
+        int[] newNums = new int[len + 2];
+        newNums[0] = newNums[len + 1] = 1;
+        for(int i = 1; i <= len; i++){
+            newNums[i] = nums[i - 1];
+        }
+
+        //dp[i][j] 表示i,j区间内最大能获得的金币
+        int[][] dp = new int[len + 2][len + 2];
+
+        //遍历顺序，这样的顺序设计是因为要先计算出已知状态，来推导后面的状态
+        //i从下到上
+        for(int i = len; i >= 0; i--){
+            //j从左到右
+            for(int j = i + 1; j < len + 2; j++){
+                //遍历最后可能戳破的气球k
+                for(int k = i + 1; k < j; k++){
+                    dp[i][j] = Math.max(
+                            dp[i][j],
+                            dp[i][k] + dp[k][j] + newNums[i] * newNums[k] * newNums[j]
+                    );
+                }
+            }
+        }
+        return dp[0][len + 1];
+    }
 }
--- src/链表反转_20221104.java
+++ src/链表反转_20221104.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class 链表反转_20221104 {
+public class 链表反转_20221104 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node6.next = node6;
+
+        reverseList(node1);
+
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = new ListNode(0);
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = pre;
+
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
 }
